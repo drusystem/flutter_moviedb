ee1dbc494919a7b30e3981180ad8ef495cf8b875
Andres Quispe <drusystem@mail.com>
2025-12-01 14:40:26 -0500

    implementacion de rutas en TAPS

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,25 +1,55 @@
 import 'package:flutter_cinema_app/presentation/screens/screens.dart';
+import 'package:flutter_cinema_app/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 // GoRouter configuration
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childview: child);
+      },
       routes: [
         GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
               path: 'movie/:id',
               name: MovieScreen.name,
               builder: (context, state) {
                 final movieId = state.pathParameters['id'] ?? 'no-id';
-                return MovieScreen(movieId: movieId,);
+                return MovieScreen(movieId: movieId);
               },
             ),
-      ]
+          ]
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
+      ],
     ),
-    
+
+    // GoRoute(
+    //   path: '/',
+    //   name: HomeScreen.name,
+    //   builder: (context, state) => const HomeScreen(childview: FavoritesView()),
+    //   routes: [
+    //     GoRoute(
+    //       path: 'movie/:id',
+    //       name: MovieScreen.name,
+    //       builder: (context, state) {
+    //         final movieId = state.pathParameters['id'] ?? 'no-id';
+    //         return MovieScreen(movieId: movieId);
+    //       },
+    //     ),
+    //   ],
+    // ),
   ],
 );

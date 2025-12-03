f88340b9c8e9b6476cd8fd6613cb5236adc74950
Andres Quispe <drusystem@mail.com>
2025-12-03 11:21:52 -0500

    rutas en menu tap manteniendo estados

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,23 +3,29 @@ import 'package:go_router/go_router.dart';
 
 // GoRouter configuration
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home/0',
   routes: [
     GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) {
+        final pageIndex = state.pathParameters['page']??'0';
+        return HomeScreen(pageIndex: int.parse(pageIndex));
+      },
       routes: [
         GoRoute(
-              path: 'movie/:id',
-              name: MovieScreen.name,
-              builder: (context, state) {
-                final movieId = state.pathParameters['id'] ?? 'no-id';
-                return MovieScreen(movieId: movieId,);
-              },
-            ),
-      ]
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(movieId: movieId);
+          },
+        ),
+      ],
     ),
-    
+    GoRoute(
+      path: '/',
+      redirect: (_,_)=>'/home/0',
+    )
   ],
 );
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  final int currentIndex;
+  const CustomBottomNavigation({super.key, required this.currentIndex});
+
+  void onItemTapped(BuildContext context, int index) {
+    context.go('/home/$index');
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      
+      currentIndex: currentIndex,
+      onTap: (value)=>onItemTapped(context,value),
       items: [
-        const BottomNavigationBarItem(icon: Icon(Icons.home_max),label: 'Inicio'),
-        const BottomNavigationBarItem(icon: Icon(Icons.label_outline),label: 'Categorias'),
-        const BottomNavigationBarItem(icon: Icon(Icons.favorite_outlined),label: 'Favoritos'),
-      ]
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.home_max),
+          label: 'Inicio',
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.label_outline),
+          label: 'Categorias',
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_outlined),
+          label: 'Favoritos',
+        ),
+      ],
     );
   }
 }
